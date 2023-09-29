ca984bef894a1648a622bcd2dbdce418e8f99b14
JaysonSales <Jayson.Sales.R@gmail.com>
2023-09-30 07:45:12 +0800

    My first push

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Pages from "./pages";
+
+export default function App() {
+  return (
+    <div className="bg-slate-900 h-screen w-screen">
+      <Pages />
+    </div>
+  );
+}
---
+++ src/pages/index.jsx
@@ -0,0 +1,9 @@
+function Pages() {
+  return (
+    <div className="bg-blue-500">
+      <p className="text-7xl">Test</p>
+    </div>
+  );
+}
+
+export default Pages;
